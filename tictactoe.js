3acaf7f4753334686376f32cd840e055781a6c78
Noah Dealmeida <noahdealmeida@gmail.com>
2019-04-28 12:28:56 -0400

    Basic running of code

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,44 @@
 console.log('tictactoe.js loaded')
+
+ttt = function(){
+    // initializing
+    console.log('initializing tic-tac-toe at '+Date())
+    startDate.textContent=Date()
+
+    ttt.prepareGame()
+}
+
+ttt.prepareGame=function(){
+    ttt.board=document.getElementById('board')
+    ttt.cellM=[]
+    for(var i = 0 ; i <3 ; i++){
+        ttt.cellM[i]=[]
+        for(var j = 0 ; j <3 ; j++){
+            ttt.board.rows[i].cells[j].textContent=`(${i},${j})`
+            ttt.cellM[i][j]=ttt.board.rows[i].cells[j]
+            ttt.cellM[i][j].onclick=ttt.clickedCell
+        }
+    }
+    //debugger
+}
+
+ttt.turn = 'X'
+
+ttt.clickedCell=function(){
+    this.textContent=ttt.turn
+    if(ttt.turn=='X'){
+        ttt.turn='O'
+    }else{
+        ttt.turn='X'
+    }
+    ttt.checkVictory()
+}
+
+ttt.checkVictory=function(){
+    // check for victory 
+}
+
+
+window.onload=ttt
+
+
